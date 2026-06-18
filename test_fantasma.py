67966642082b5ea667057ad3593bc61fa7475636
Wilner Manzanares "Michego Takoro <154541788+Shuuida@users.noreply.github.com>
2026-06-17 21:12:03 -0400

    Delete test_fantasma.py

--- test_fantasma.py
+++
@@ -1,8 +0,0 @@
-def funcion_que_sera_borrada():
-    print("Hola. Solo soy un fantasma temporal de prueba para Vex.")
-    return True
-
-
-class ClaseDescartable:
-    def hacer_nada(self):
-        pass

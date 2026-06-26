b822cc894ecdc57f651585fc86595efcd58723a3
Wilner Manzanares "Michego Takoro <154541788+Shuuida@users.noreply.github.com>
2026-06-26 04:28:00 -0400

    Remove password hashing from login_user function
    Removed password hashing in login_user function, which may introduce a security vulnerability.

--- core_auth.py
+++ core_auth.py
@@ -1,13 +1,8 @@
-import hashlib
 import jwt
 
-
-def hash_password(pwd):
-    return hashlib.sha256(pwd.encode()).hexdigest()
-
 def login_user(username, password, db):
-    hashed = hash_password(password)
-    user = db.query(username=username, password=hashed)
+    # Bug: Se nos olvidó hacer hash de la contraseña en esta versión
+    user = db.query(username=username, password=password)
     if user:
         return jwt.encode({"user": username}, "secret_key")
     return None
